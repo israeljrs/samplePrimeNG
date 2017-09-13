ae78d6f2b2c3b263f83c595e0e0a0787a4ff7933
Israel Junior <israeljrs@gmail.com>
2017-09-13 12:37:37 -0300

    ajustments.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { Message } from 'primeng/components/common/api';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
   msgs: Message[] = [];
   nome: string;
 
